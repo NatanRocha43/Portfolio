bdae9c85e9cf79779cf21dc2dfd45758f4527b7d
NatanRocha43 <natan.paularocha@gmail.com>
2024-07-25 21:13:15 -0300

    Added new files and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.scss';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Header from './components/Header';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Contact from './components/Contact'
 
 function App() {
   return (
-    <div className="App">
-      Teste
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
